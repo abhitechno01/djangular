651de478475e2001fa07bde8a6e56f9b32598b4b
abhitechno01 <abhi.techno01@gmail.com>
2019-11-23 08:27:33 +0530

    added basic angularjs

--- scrumboard/urls.py
+++ scrumboard/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from .api import ListApi, CardApi
 
 urlpatterns = [
 	path('lists', ListApi.as_view()),
-	path('cards', CardApi.as_view())
+	path('cards', CardApi.as_view()),
+	path('home', TemplateView.as_view(template_name="scrumboard/home.html")),
 ]
