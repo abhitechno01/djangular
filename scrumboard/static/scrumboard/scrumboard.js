40c977d319ad98b20e4d9c1033e78c49e248b270
abhitechno01 <abhi.techno01@gmail.com>
2019-11-27 07:37:54 +0530

    removed anulgarjs dummy data and implemented http service

--- scrumboard/static/scrumboard/scrumboard.js
+++ scrumboard/static/scrumboard/scrumboard.js
@@ -1,50 +1,23 @@
 (function(){
 			'use strict';
 			angular.module('scrumboard.demo',[])
-			.controller('ScrumboardController', ['$scope',ScrumboardController]);
-			function ScrumboardController($scope){
+			.controller('ScrumboardController', ['$scope','$http',ScrumboardController]);
+			function ScrumboardController($scope, $http){
 				
 				$scope.add = function(list, title){
 					var card = {
 						title:title
 					};
 					list.cards.push(card);
-				}
-
-				$scope.data = [
-				{
-					name:'Django demo',
-					cards: [
-						{
-							title:'Create models'
-						},
-						{
-							title:'Create view'
-						},
-						{
-							title:'Migrate database'
-						},
-					]
-				},
-				{
-					name: 'Angular demo',
-					cards:[
-						{
-							title:'Write html'
-						},
-						{
-							title:'Create controller'
-						},
-						{
-							title:'Create directives'
-						}
-					]
-				}
-				];
-
-				$scope.person = {
-					'name': 'Joe',
-					'age':35
 				};
+
+				$scope.data = [];
+				
+				$http.get('/scrumboard/lists')
+				.then(function(response){
+					$scope.data =response.data;
+				});
+
+				
 			}
 		}());
