f760622be0d296babf4e7775d495fab58b365e14
abhitechno01 <abhi.techno01@gmail.com>
2019-11-28 07:16:36 +0530

    using post api in anuglarjs

--- scrumboard/static/scrumboard/scrumboard.js
+++ scrumboard/static/scrumboard/scrumboard.js
@@ -6,14 +6,23 @@
 				
 				$scope.add = function(list, title){
 					var card = {
+						list: list.id,
 						title:title
 					};
+					$http.post('/scrumboard/cards/', card)
+					.then(function(response){
+
 					list.cards.push(card);
+					},
+					function(){
+						alert('Could not create card');
+					}
+					);
 				};
 
 				$scope.data = [];
 				
-				$http.get('/scrumboard/lists')
+				$http.get('/scrumboard/lists/')
 				.then(function(response){
 					$scope.data =response.data;
 				});
