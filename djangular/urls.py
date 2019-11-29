6739d2ce44dab2514ca961955c1c4c1684d47a41
abhitechno01 <abhi.techno01@gmail.com>
2019-11-29 07:48:42 +0530

    reorganized codebase

--- djangular/urls.py
+++ djangular/urls.py
@@ -21,6 +21,6 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('scrumboard/', include('scrumboard.urls')),
-    path('', ensure_csrf_cookie(TemplateView.as_view(template_name="scrumboard/home.html"))),
+    path('', ensure_csrf_cookie(TemplateView.as_view(template_name="home.html"))),
     path('auth_api/', include('auth_api.urls'))
 ]
