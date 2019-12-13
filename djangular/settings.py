66d20d14797ee138ba073200fda70583c0449c2e
abhitechno01 <abhi.techno01@gmail.com>
2019-12-13 21:38:05 +0530

    added gunicorn but couldn't configure it properly

--- djangular/settings.py
+++ djangular/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6hz4yoz%1zg@e-dte*ttfos+^nb$s+c)5)x!xgfn(v@1g6#dn_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangular.local', 'localhost']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
